d1cb1f1f5362ed75c783d8360a5d67a726304d9c
Koji-Study <56583902+Koji-Study@users.noreply.github.com>
2023-05-15 11:24:01 +0800

    Create text-to-speech.py

---
+++ text-to-speech.py
@@ -0,0 +1,19 @@
+# ! -*- coding: utf-8 -*-
+import pyttsx3
+
+def voice():
+    engine = pyttsx3.init()
+    #设置语速
+    engine.setProperty('rate', 130)
+    #设置音量
+    engine.setProperty('volume', 0.5)
+    voices = engine.getProperty('voices')
+    #设置第一个语音合成器
+    engine.setProperty('voice', voices[0].id)
+    for num in range(3):
+        engine.say("新的运维时间产生，请及时处理")
+    engine.runAndWait()
+    engine.stop()
+
+if __name__ == '__main__':
+    voice()

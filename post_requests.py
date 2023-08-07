5b837bb5eb559dd08cc4b2724e372b358524cd22
Koji-Study <56583902+Koji-Study@users.noreply.github.com>
2023-08-07 18:14:34 +0800

    Create post_requests.py

---
+++ post_requests.py
@@ -0,0 +1,26 @@
+import requests
+
+def call_api():
+    #API URL
+    url = "https://api.example.com"
+    #要发送的数据
+    data = {
+        "key1": "value1",
+        "key2": "value2"
+    } 
+    try:
+        response = requests.post(url, json=data)
+        # 处理API成功响应
+        if response.status_code == 200:
+            
+            print("API Response:")
+            print(response.json())
+        # 处理API错误响应
+        else:
+            print(f"API returned error: {response.status_code} - {response.text}")
+    # 处理请求异常
+    except requests.exceptions.RequestException as e:
+        print(f"Error making API request: {e}")
+
+if __name__ == "__main__":
+    call_api()
